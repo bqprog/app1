9d928dc0e81272a015a495836af536e93d717904
bqprog <bossmolokososa@mail.ru>
2025-08-03 19:12:42 +0300

    начинаем создавать goods

--- main/views.py
+++ main/views.py
@@ -4,12 +4,19 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title' : 'Home',
-        'content' :'Главная страница магазина - HOME' 
+        'title' : 'Home - Главная',
+        'content' : 'Магазин мебели HOME'
+        
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('about page ')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        "text_on_page" : "текст о том почему магазин такой крутой"
+    }
+
+    return render(request, 'main/about.html', context)
